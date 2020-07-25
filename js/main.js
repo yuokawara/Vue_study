cc28d2b0c72971e897896bae3c11465c9a7323ce
Yu Okawara <solbonta@hotmail.com>
2020-07-25 21:17:07 +0900

    Vue練習 tutorial 1 20200725

--- js/main.js
+++ js/main.js
@@ -1,6 +1,13 @@
 var app = new Vue({
     el: '#app',
     data: {
-        message: 'hello vue'
+        message: 'hello vue',
+        count: 0,
+        user: {
+            LastName: 'Ninja',
+            FirstName: 'Fujikido',
+            City: 'Saitama',
+        },
+        colors: ['Red', 'blue', 'Green'],
     }
 })
