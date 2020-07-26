59892615de078f0a1475345dbbf4f22982fdc45d
Yu Okawara <solbonta@hotmail.com>
2020-07-26 11:12:28 +0900

    Vue練習中 tutorial 2 20200726

--- js/main.js
+++ js/main.js
@@ -1,13 +1,37 @@
+// コンポーネントの練習 Vue.component(コンポーネント名, コンポーネント定義)
+Vue.component('hello-conponent', {
+    // templateを定義
+    template: '<p>Hello</p>',
+})
+
+
+
 var app = new Vue({
     el: '#app',
     data: {
+        // tutorial
         message: 'hello vue',
-        count: 0,
+        toggle: true,
+        toggle2: true,
+        languages: ['Ruby', 'Python', 'Dart'],
+
+        // v-for
         user: {
-            LastName: 'Ninja',
-            FirstName: 'Fujikido',
-            City: 'Saitama',
+            firstName: 'Fujikido',
+            lastName: 'NinjaSlayer',
+            age: 30,
         },
-        colors: ['Red', 'blue', 'Green'],
-    }
+
+        // v-on
+        now: '',
+    },
+    // eventhandler ボタンを押したら時刻表示の処理
+    methods: {
+        onclick: function () {
+            // alert('ボタン押したよー')
+            this.now = new Date().toLocaleString();
+            this.message = '変わったよー';
+            this.languages = ['Java', 'HTML', 'CSS','Go']
+        }
+    },
 })
