88e4c567dc053429b0550db798c25d8530c85949
Yu Okawara <solbonta@hotmail.com>
2020-07-28 15:06:51 +0900

    vue練習中 todo終了 20200728

--- todo/js/main.js
+++ todo/js/main.js
@@ -1,12 +1,31 @@
 var app = new Vue({
     el: '#app',
     data: {
-        newEvent: ''
+        newEvent: '',
+        events: [],
+        now: '',
     },
     methods: {
         addEvent: function (event) {
-            // alert('testだよ')
-            
+            if (this.newEvent == '') return alert('empty');
+            var event = {
+                item: this.newEvent,
+                isDone: false,
+
+            };
+            this.events.push(event);
+            this.newEvent = '';
+
+            this.now = new Date().toLocaleString();
+        },
+
+        deleteEvent: function (index) {
+            // alert(index, 'is this delete?');
+            this.events.splice(index, 1);
+        },
+
+        alldeleteEvent: function (index) {
+            this.events.splice(index);
         }
     },
 })
