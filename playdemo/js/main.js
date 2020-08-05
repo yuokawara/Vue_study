9634c1eb1245efe766c1c8ed4117bfd7817e2356
Yu Okawara <solbonta@hotmail.com>
2020-08-06 08:09:57 +0900

    vue学習中 フィルターの設定 20200806

--- playdemo/js/main.js
+++ playdemo/js/main.js
@@ -1,3 +1,15 @@
+Vue.filter('toUSD', function(jpy) {
+    return jpy / 100
+})
+
+Vue.filter('numberFormat', function(value) {
+    return value.toLocaleString()
+})
+
+Vue.filter('readMore', function(text, length, suffix) {
+    return text.substring(0, length) + suffix
+})
+
 var app = new Vue({
     el: '#app',
     data: {
@@ -7,7 +19,15 @@ var app = new Vue({
         message3: 'V-textの表示テスト中',
         number: 100,
         ok: true,
+        price: 30000,
+        jpyprice: 3150000,
+        text: 'In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document or a typeface without relying on meaningful content.',
     },
+    // filters: {
+    //     numberFormat: function (value) {
+    //         return value.toLocaleString()
+    //     }
+    // },
     methods: {
         clickHandler: function(event) {
             this.message = this.message.split('').reverse().join('')
