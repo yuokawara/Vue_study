7b9d6c646b20c86c008fc4261dddc3cf8533351d
Yu Okawara <solbonta@hotmail.com>
2020-08-20 08:54:18 +0900

    vue練習中 練習終了 20200820

---
+++ study-vuerouter/my-first-cli-project/src/views/Home.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="home">
+    <HelloNuxt/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+// import HelloWorld from '@/components/HelloWorld.vue'
+import HelloNuxt from '@/components/HelloNuxt.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    // HelloWorld,
+    HelloNuxt
+  }
+}
+</script>
